27755de0fbb802bef1ac397a6c49c75633b73b28
Szabolcs Balogh <bszabolcs@gmail.com>
2015-09-17 20:27:00 +0300

    Do not deactivate audio profile because it break AudioContext in JavaScript

--- src/ios/CDVSound.m
+++ src/ios/CDVSound.m
@@ -370,7 +370,7 @@
         NSLog(@"Failed to initialize AVAudioPlayer: %@\n", [playerError localizedDescription]);
         audioFile.player = nil;
         if (self.avSession) {
-            [self.avSession setActive:NO error:nil];
+//            [self.avSession setActive:NO error:nil];
         }
         bError = YES;
     } else {
@@ -461,7 +461,7 @@
                 [audioFile.recorder stop];
             }
             if (self.avSession) {
-                [self.avSession setActive:NO error:nil];
+//                [self.avSession setActive:NO error:nil];
                 self.avSession = nil;
             }
             [[self soundCache] removeObjectForKey:mediaId];
@@ -553,7 +553,7 @@
                 }
                 audioFile.recorder = nil;
                 if (self.avSession) {
-                    [self.avSession setActive:NO error:nil];
+//                    [self.avSession setActive:NO error:nil];
                 }
                 jsString = [NSString stringWithFormat:@"%@(\"%@\",%d,%@);", @"cordova.require('cordova-plugin-media.Media').onStatus", mediaId, MEDIA_ERROR, [self createMediaErrorWithCode:MEDIA_ERR_ABORTED message:errorMsg]];
                 [self.commandDelegate evalJs:jsString];
@@ -573,7 +573,7 @@
                     NSLog(@"%@", msg);
                     audioFile.recorder = nil;
                     if (self.avSession) {
-                        [self.avSession setActive:NO error:nil];
+//                        [self.avSession setActive:NO error:nil];
                     }
                     jsString = [NSString stringWithFormat:@"%@(\"%@\",%d,%@);", @"cordova.require('cordova-plugin-media.Media').onStatus", mediaId, MEDIA_ERROR, [self createMediaErrorWithCode:MEDIA_ERR_ABORTED message:msg]];
                     [self.commandDelegate evalJs:jsString];
@@ -661,7 +661,7 @@
         jsString = [NSString stringWithFormat:@"%@(\"%@\",%d,%@);", @"cordova.require('cordova-plugin-media.Media').onStatus", mediaId, MEDIA_ERROR, [self createMediaErrorWithCode:MEDIA_ERR_DECODE message:nil]];
     }
     if (self.avSession) {
-        [self.avSession setActive:NO error:nil];
+//        [self.avSession setActive:NO error:nil];
     }
     [self.commandDelegate evalJs:jsString];
 }
@@ -684,7 +684,7 @@
         jsString = [NSString stringWithFormat:@"%@(\"%@\",%d,%@);", @"cordova.require('cordova-plugin-media.Media').onStatus", mediaId, MEDIA_ERROR, [self createMediaErrorWithCode:MEDIA_ERR_DECODE message:nil]];
     }
     if (self.avSession) {
-        [self.avSession setActive:NO error:nil];
+//        [self.avSession setActive:NO error:nil];
     }
     [self.commandDelegate evalJs:jsString];
 }
